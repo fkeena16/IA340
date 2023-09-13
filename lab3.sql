bfe6f31f5f95f4248e3326ec71a53f8fa13857cc
fkeena16 <143033821+fkeena16@users.noreply.github.com>
2023-09-13 15:48:00 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -11,13 +11,15 @@ select * from course;
 --q2.2
  --insert into professor(p_email,p_name,office)
 -- values('p1@jmu.edu','p1','o1'),
---('p2@jmu.edu','p2','o2');
+--('p2@jmu.edu','p2','o2'),
+--('p3@jmu.edu', 'p3', o3)
 
 --q2.3
 -- insert into course(c_number,c_name,room,p_email)
  --values('c1','postgresql','r1','p1@jmu.edu'),
 -- ('c2','mongodb','r2','p2@jmu.edu'),
--- ('c3','twitter','r1','p1@jmu.edu');
+-- ('c3','twitter','r1','p1@jmu.edu'),
+--('c4','facebook','r1','p3@jmu.edu')
 
 --q2.4
 --insert into enroll(s_email,c_number)
